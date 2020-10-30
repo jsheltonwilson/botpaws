ae1d4898d526c42e7e4a8acc4deb84a494d66b79
jsheltonwilson <sheltonwilson911@gmail.com>
2020-10-29 20:02:39 -0400

    added spotify stalking

--- bot.py
+++ bot.py
@@ -294,12 +294,12 @@ async def poll(ctx, *, question):
     await sentPoll.add_reaction(emoji= moofrown)
 
 @bot.command(help=" spotify command to show currently playing song if unable to see due to other activity blocking it")
-async def spotify(ctx, *, member: discord.Member=None):
-    if member is None:
-        member = ctx.message.author
+async def spotify(ctx, member: discord.Member=None):
+    
+    member = member or ctx.author
     
     for activity in member.activities:
-        if isinstance(activity, discord.Spotify):
+        if isinstance(activity, Spotify):
             await ctx.channel.send(f"{member.display_name} is listening to {activity.title} by {activity.artist}")
         else: 
             await ctx.channel.send(f"{member.display_name} is not currently listening to music (weirdo).")
