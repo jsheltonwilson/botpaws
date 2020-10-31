5342732f17dd5ccbe347cc7829e606101b5b0a18
jsheltonwilson <sheltonwilson911@gmail.com>
2020-10-31 13:58:30 -0400

    test

--- bot.py
+++ bot.py
@@ -16,7 +16,6 @@ with open("./config.yml", "r", encoding = "utf-8") as file:
 
 
 intents = discord.Intents().all()
-intents.members = False
 bot = commands.Bot(command_prefix =config['Prefix'] , description = "speedy paw bot created by speediest paws", intents=intents)
 bot.startTime = datetime.datetime.now(datetime.timezone.utc)
 tChannelID = config['test Channel ID']
