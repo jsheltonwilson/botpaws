9bdca758f37250c4fb75e2c463fb3b37f437145a
jsheltonwilson <sheltonwilson911@gmail.com>
2020-11-01 12:52:29 -0500

    spotify thing

--- bot.py
+++ bot.py
@@ -15,8 +15,8 @@ with open("./config.yml", "r", encoding = "utf-8") as file:
 
 
 
-intents = discord.Intents.default()
-intents.presences = True
+intents = discord.Intents.all()
+#intents.presences = True
 bot = commands.Bot(command_prefix =config['Prefix'] , description = "speedy paw bot created by speediest paws", intents=intents)
 bot.startTime = datetime.datetime.now(datetime.timezone.utc)
 tChannelID = config['test Channel ID']
